07d7e52adc4f0eca1c188e3206558d0fd2f45247
fayasPA <fayasmuthalib@gmail.com>
2024-11-11 15:35:27 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './pages/Home';
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+         <Route path="" element={<Home />} />
+         {/* <Route path="/products" element={<Products />} />
+         <Route path="/blog" element={<Blog />} />
+         <Route path="/contact" element={<Contacts />} />
+         <Route path="/about" element={<About />} /> */}
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layout.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+
+
+  return (
+    <div
+      className="your-main-container bg-contain md:bg-contain bg-center min-h-screen flex flex-col"
+    >
+        <div className="bg-selBlack/90 flex-grow md:pb-0">
+          <Navbar />
+
+          <main className="flex-grow">
+            <Outlet />
+          </main>
+
+          <Footer />
+
+        </div>
+    </div>
+  );
+};
+
+export default Layout;
